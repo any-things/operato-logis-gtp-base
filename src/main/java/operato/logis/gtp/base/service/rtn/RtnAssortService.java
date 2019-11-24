8d1489a7a3ddcc11157f1db009776e1c3118463a
hatiolab <shortstop@hatiolab.com>
2019-11-24 23:06:47 +0900

    ...

--- src/main/java/operato/logis/gtp/base/service/rtn/RtnAssortService.java
+++ src/main/java/operato/logis/gtp/base/service/rtn/RtnAssortService.java
@@ -24,14 +24,13 @@ import xyz.anythings.base.service.impl.LogisServiceDispatcher;
 import xyz.anythings.sys.service.AbstractExecutionService; 
 import xyz.anythings.sys.util.AnyOrmUtil;
 import xyz.elidom.dbist.dml.Query;
-import xyz.elidom.exception.server.ElidomRuntimeException; 
+import xyz.elidom.exception.server.ElidomRuntimeException;
+import xyz.elidom.sys.SysConstants;
 import xyz.elidom.util.DateUtil;
 import xyz.elidom.util.ValueUtil; 
 
-
-
 /**
- * 반품 분류 서비스  구현
+ * 반품 분류 서비스 구현
  *
  * @author shortstop
  */
@@ -53,7 +52,23 @@ public class RtnAssortService extends AbstractExecutionService implements IAssor
 	public JobConfigSet getJobConfigSet(String batchId) {
 		return this.serviceDispatcher.getConfigSetService().getConfigSet(batchId);
 	}
+	
+	@Override
+	public IBoxingService getBoxingService(Object... params) {
+		return null;
+	}
 
+	@Override
+	public void batchStartAction(JobBatch batch) {
+		// TODO 작업 지시 시점에 반품 관련 추가 처리 - Ex) 할당된 표시기에 점등 ... 
+	}
+
+	@Override
+	public void batchCloseAction(JobBatch batch) {
+		// TODO 배치 마감 시점에 반품 관련 추가 처리 - Ex) 셀 별 잔량 풀 박스 처리 ... 
+		this.serviceDispatcher.getIndicationService(batch).indicatorOffAll(batch.getDomainId(), batch.getId());
+	}
+	
 	@Override
 	public Category categorize(ICategorizeEvent event) {
 		// TODO 중분류 
@@ -68,38 +83,31 @@ public class RtnAssortService extends AbstractExecutionService implements IAssor
 
 	@Override
 	public Object input(IClassifyInEvent inputEvent) {
-		// TODO 소분류 - 상품 투입 
 		return this.inputSkuSingle(inputEvent); 
 		
 	} 
 	
-	@EventListener(classes = ClassifyRunEvent.class, condition = "#event.jobType == 'rtn")
+	@EventListener(classes = ClassifyRunEvent.class, condition = "#event.jobType == 'rtn'")
 	public Object classify(IClassifyRunEvent exeEvent) { 
-		String classifyAction = exeEvent.getClassifyAction();
-		JobInstance instance = exeEvent.getJobInstance(); 
 		
-		Integer reqQty = exeEvent.getReqQty();//피킹 수량
-		Integer resQty = exeEvent.getResQty();//확정 수량 
-		Integer picking = 0, picked=0;
-		
-		switch(classifyAction) {
+		switch(exeEvent.getClassifyAction()) {
 			case LogisCodeConstants.CLASSIFICATION_ACTION_CONFIRM :
 				this.confirmAssort(exeEvent);
-			break; 
+				break;
+				
 			case LogisCodeConstants.CLASSIFICATION_ACTION_CANCEL :
 				this.cancelAssort(exeEvent);
-				break; 
+				break;
+				
 			case LogisCodeConstants.CLASSIFICATION_ACTION_MODIFY :
 				this.splitAssort(exeEvent);
 				break;
+				
 			case LogisCodeConstants.CLASSIFICATION_ACTION_FULL :
 				this.fullBoxing(exeEvent);
-				break;
-				
-			default :
-				break;
 		}
-		return null;
+		
+		return exeEvent.getJobInstance();
 	}
 
 	@Override
@@ -109,75 +117,40 @@ public class RtnAssortService extends AbstractExecutionService implements IAssor
 	}
 
 	@Override
-	public boolean checkEndClassifyAll(String batchId) {
-		// TODO Auto-generated method stub
-		return false;
-	}
-
-	@Override
-	public void batchStartAction(JobBatch batch) {
-		// TODO 작업 지시 시점에 반품 관련 추가 처리 - Ex) 할당된 표시기에 점등 ... 
-		
-	}
-
-	@Override
-	public void batchCloseAction(JobBatch batch) {
-		// TODO 배치 마감 시점에 반품 관련 추가 처리 - Ex) 셀 별 잔량 풀 박스 처리 ... 
-		
-	}
-
-	@Override
-	public void handleClassifyException(IClassifyErrorEvent errorEvent) {
-		// TODO Auto-generated method stub
-		
-	}
-
-	@Override
-	public IBoxingService getBoxingService(Object... params) {
-		// TODO Auto-generated method stub
-		return null;
-	}
-
-	@Override
 	public Object inputSkuSingle(IClassifyInEvent inputEvent) {
 		JobBatch batch = inputEvent.getJobBatch(); 
-		String comCd = inputEvent.getComCd();
-		String skuCd = inputEvent.getInputCode();
-		Integer inputQty = inputEvent.getInputQty();
-		String nowStr = DateUtil.currentTimeStr();
 		
 		Query condition = AnyOrmUtil.newConditionForExecution(batch.getDomainId());
 		condition.addFilter("batchId", batch.getId());
-		condition.addFilter("comCd", comCd);
-		condition.addFilter("skuCd", skuCd);
 		condition.addFilter("equipCd", batch.getEquipCd());
-		condition.addFilter("status", "in", LogisConstants.JOB_STATUS_WIPC);
+		condition.addFilter("comCd", inputEvent.getComCd());
+		condition.addFilter("skuCd", inputEvent.getInputCode());
+		condition.addFilter("status", SysConstants.IN, LogisConstants.JOB_STATUS_WIPC);
 		JobInstance instance = this.queryManager.selectByCondition(JobInstance.class, condition);
-		
-		Integer pickingQty = instance.getPickingQty()+inputQty;
+		int pickingQty = instance.getPickingQty() + inputEvent.getInputQty();
 	 
-		if(instance.getPickQty() > pickingQty)
-		{	
-			//배치의 Max Seq 사용
-			instance.setInputSeq(1);
+		if(instance.getPickQty() > pickingQty) {
+			int lastInputSeq = this.serviceDispatcher.getJobStatusService(batch).findNextInputSeq(batch);
+			instance.setInputSeq(lastInputSeq);
 			instance.setPickingQty(pickingQty);
 			instance.setStatus(LogisConstants.JOB_STATUS_PICKING);
-			if(ValueUtil.isEmpty(instance.getPickStartedAt()))
-			{
+			instance.setColorCd(LogisConstants.COLOR_RED);
+			String nowStr = DateUtil.currentTimeStr();
+			
+			if(ValueUtil.isEmpty(instance.getPickStartedAt())) {
 				instance.setPickStartedAt(nowStr);
 			}
 			
-			if(ValueUtil.isEmpty(instance.getInputAt()))
-			{
+			if(ValueUtil.isEmpty(instance.getInputAt())) {
 				instance.setInputAt(nowStr);
 			} 
-			instance.setColorCd(LogisConstants.COLOR_RED);
-			this.queryManager.update(instance, "inputSeq", "pickingQty", "status", "pickStartedAt", "inputAt", "colorCd");
+			
+			this.queryManager.update(instance, "inputSeq", "pickingQty", "status", "colorCd", "pickStartedAt", "inputAt", "updatedAt");
 			
 			// 표시기 점등
 			this.serviceDispatcher.getIndicationService(batch).indicatorOnForPick(instance, instance.getPickingQty(), 0, 0);
 			
-		}else {
+		} else {
 			// 처리 가능한 수량 초과.
 			throw new ElidomRuntimeException("처리 예정 수량을 초과 했습니다.");
 		} 
@@ -205,65 +178,63 @@ public class RtnAssortService extends AbstractExecutionService implements IAssor
 
 	@Override
 	public void confirmAssort(IClassifyRunEvent exeEvent) {
-		int reqQty = exeEvent.getReqQty(); //불켜진 수량
-		int resQty = exeEvent.getResQty(); // 확정 수량
-		
 		JobInstance job = exeEvent.getJobInstance();
 		job.setPickingQty(0);
-		job.setPickedQty(job.getPickedQty() + resQty);
+		job.setPickedQty(job.getPickedQty() + exeEvent.getResQty());
 		
 		if(job.getPickedQty() >= job.getPickQty()) {
 			job.setStatus(LogisConstants.JOB_STATUS_FINISH);
-			this.queryManager.update(job, "pickingQty", "pickedQty","status");
-		}else {
-			this.queryManager.update(job, "pickingQty", "pickedQty");
-		} 
+			job.setPickEndedAt(DateUtil.currentTimeStr());
+		}
+		
+		this.queryManager.update(job, "pickingQty", "pickedQty", "status", "pickEndedAt", "updatedAt");
+		this.afterClassifiedFinish(exeEvent);
 	}
 
 	@Override
 	public void cancelAssort(IClassifyRunEvent exeEvent) {
-		int reqQty = exeEvent.getReqQty(); //불켜진 수량
-		int resQty = exeEvent.getResQty(); // 확정 수량
-		JobInstance job = exeEvent.getJobInstance();
-		
+		JobInstance job = exeEvent.getJobInstance();		
 		job.setPickingQty(0);
-		
-		this.queryManager.update(job, "pickingQty");
+		this.queryManager.update(job, "pickingQty", "updatedAt");
+		this.afterClassifiedFinish(exeEvent);
 	}
 
 	@Override
 	public int splitAssort(IClassifyRunEvent exeEvent) {
-		int reqQty = exeEvent.getReqQty(); //불켜진 수량
-		int resQty = exeEvent.getResQty(); // 확정 수량
-		
 		JobInstance job = exeEvent.getJobInstance();
 		job.setPickingQty(0);
-		job.setPickedQty(job.getPickedQty() + resQty);
+		job.setPickedQty(job.getPickedQty() + exeEvent.getResQty());
 		
 		if(job.getPickedQty() >= job.getPickQty()) {
 			job.setStatus(LogisConstants.JOB_STATUS_FINISH);
-			this.queryManager.update(job, "pickingQty", "pickedQty","status");
-		}else {
-			this.queryManager.update(job, "pickingQty", "pickedQty");
 		} 
-		return 0;
+		
+		this.queryManager.update(job, "pickingQty", "pickedQty", "status", "updatedAt");
+		this.afterClassifiedFinish(exeEvent);
+		return exeEvent.getResQty();
 	}
 
 	@Override
 	public int undoAssort(IClassifyRunEvent exeEvent) {
 		// TODO Auto-generated method stub
+		
+		this.afterClassifiedFinish(exeEvent);
 		return 0;
 	}
 
 	@Override
 	public BoxPack fullBoxing(IClassifyRunEvent exeEvent) {
 		// TODO Auto-generated method stub
+		
+		this.afterClassifiedFinish(exeEvent);
 		return null;
 	}
 
 	@Override
 	public BoxPack partialFullboxing(IClassifyRunEvent exeEvent) {
 		// TODO Auto-generated method stub
+		
+		this.afterClassifiedFinish(exeEvent);
 		return null;
 	}
 
@@ -292,11 +263,42 @@ public class RtnAssortService extends AbstractExecutionService implements IAssor
 	}
 
 	@Override
-	public boolean finishAssortCell(JobInstance job, WorkCell workCell, boolean finalEndFlag) {
+	public boolean checkEndClassifyAll(String batchId) {
 		// TODO Auto-generated method stub
 		return false;
 	}
 	
-	
-	
+	@Override
+	public boolean finishAssortCell(JobInstance job, WorkCell workCell, boolean finalEndFlag) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public void handleClassifyException(IClassifyErrorEvent errorEvent) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	/**
+	 * 분류 처리가 완료된 이후 처리
+	 * 
+	 * @param exeEvent
+	 */
+	private void afterClassifiedFinish(IClassifyRunEvent exeEvent) {
+		JobInstance job = exeEvent.getJobInstance();
+		
+		// 셀 분류가 모두 완료되었다면 ...
+		if(this.checkCellAssortEnd(exeEvent.getJobBatch(), null, job)) {
+			Query condition = AnyOrmUtil.newConditionForExecution(exeEvent.getDomainId());
+			condition.addFilter("batchId", job.getBatchId());
+			condition.addFilter("cellCd", job.getSubEquipCd());
+			WorkCell cell = this.queryManager.selectByCondition(WorkCell.class, condition);
+			this.finishAssortCell(exeEvent.getJobInstance(), cell, false);
+			
+		} else {
+			// TODO 표시기에 셀의 잔량 수량 표시 ... 
+		}
+	}
+
 }
